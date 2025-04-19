ab986ed2bfcc2134bd94f4448a3acaeb7edb65aa
Raja1971 <pami6002@gmail.com>
2025-04-19 20:43:16 +0530

    Add files via upload

---
+++ My_Own_Code1904.py
@@ -0,0 +1,18 @@
+#---------------------------------------------------------------
+# ✨ My First Ever Python Program ✨
+# Created on: 19 - 04 - 2025
+# Coded by: T.Raja
+# Purpose: To check if a number is even or odd
+# All glory, praise, and honor to our Lord Jesus Christ 🙌
+# ---------------------------------------------------------------
+
+# Taking input from user and converting it to integer
+num = int(input(" Enter The  Number  = "))
+print("The Input Number is : ", num)
+if num % 2 == 0 :
+        print(f"The Number : {num} is Even.")
+else:
+        print(f"The Number  : {num} is Odd.")
+
+print(" Own  Progarm Created on 19 - 04 - 2025 ")
+print(" Praise alway be to our Lord Jesus Christ")
